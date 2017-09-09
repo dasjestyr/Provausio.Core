e4b1c7d709df34d033338dc6427c5efe812b0bde
Jeremy Stafford <draepetan@live.com>
2017-09-09 14:11:55 -0700

    Added crypto lib

---
+++ src/Provausio.Crypto/HashedKey.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Provausio.Crypto
+{
+    /// <summary>
+    /// Helper class for predefining an objects elements for generating a hash.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class HashedKey<T>
+    {
+        private readonly object _seed;
+        private readonly Expression<Func<T, string>>[] _properties;
+
+        public HashedKey(params Expression<Func<T, string>>[] properties)
+        {
+            _properties = properties;
+        }
+
+        public HashedKey(object seed, params Expression<Func<T, string>>[] properties)
+            : this(properties)
+        {
+            _seed = seed;
+        }
+
+        public string GetBase64(T source, bool urlEncode = true)
+        {
+            var data = GetBytes(source);
+            var base64 = Convert.ToBase64String(data);
+            return urlEncode
+                ? Base64UrlEncoder.Encode(base64)
+                : base64;
+        }
+
+        public byte[] GetBytes(T source)
+        {
+            var data = GetHashValue(source);
+
+            var seedHash = _seed?.GetHashCode();
+            var seedValue = seedHash.HasValue
+                ? BitConverter.ToUInt32(BitConverter.GetBytes(seedHash.Value), 0)
+                : 0U;
+
+            return MurmurHasher.GetHash(data, seedValue);
+        }
+
+        public Guid GetId(T source)
+        {
+            var bytes = GetBytes(source);
+            return new Guid(bytes);
+        }
+
+        public bool AreEqual(T left, T right)
+        {
+            return GetId(left) == GetId(right);
+        }
+
+        private string GetHashValue(T source)
+        {
+            var values = _properties
+                .Select(property => property.Compile()(source))
+                .ToList();
+
+            return string.Join("|", values);
+        }
+    }
+}
---
+++ src/Provausio.Crypto/MurmurHasher.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace Provausio.Crypto
+{
+    /// <summary>
+    /// Static wrapper for the murmur hashing library.
+    /// </summary>
+    public static class MurmurHasher
+    {
+        public static Guid GetGuid(string data, object seed)
+        {
+            return new Guid(GetHash(data, seed));
+        }
+
+        public static byte[] GetHash(string data, object seed)
+        {
+            var seedHash = seed.GetHashCode();
+            var seedValue = BitConverter.ToUInt32(BitConverter.GetBytes(seedHash), 0);
+
+            var dataBytes = Encoding.UTF8.GetBytes(data);
+            var alg = Murmur.MurmurHash.Create128(seedValue);
+            var hashBytes = alg.ComputeHash(dataBytes);
+
+            return hashBytes;
+        }
+
+        public static Guid GetGuid(string data)
+        {
+            return new Guid(GetHash(data));
+        }
+
+        public static byte[] GetHash(string data)
+        {
+            var dataBytes = Encoding.UTF8.GetBytes(data);
+            var alg = Murmur.MurmurHash.Create128();
+            var hashBytes = alg.ComputeHash(dataBytes);
+
+            return hashBytes;
+
+        }
+    }
+}
